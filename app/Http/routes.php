5c9e067676449373b6b6974bef98ade635e694bf
Alerbethe <2458360373@qq.com>
2017-06-13 07:38:39 +0000

    finish user signup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,3 +3,4 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/find', 'StaticPagesController@find')->name('find');
 Route::get('/theme', 'StaticPagesController@theme')->name('theme');
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::resource('users', 'UsersController');
